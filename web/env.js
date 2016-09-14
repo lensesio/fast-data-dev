418a487fc4f7ed282a03529787bf3e3770691bc5
Christina Daskalaki <chdask@gmail.com>
2016-09-14 11:35:29 +0100

    move vars to env

---
+++ web/env.js
@@ -0,0 +1,47 @@
+var runningServices = [
+    {
+      "name" : "Confluent v3.0.1 - Kafka v0.10.0.1",
+      "description" : "1 broker, including kafka connect distributed, schema registry, kafka rest, etc"
+    },
+    {
+      "name" : "Datamountaineer Stream Reactor v0.2.2",
+      "description" : "Source & Sink connectors supporting KCQL. ElasticSearch, Cassandra, Redis etc"
+    },
+    {
+      "name" : "Landoop Fast Data platform v0.7",
+      "description" : "Manage schemas, view history, browse topics & configure your cluster from your browser."
+    }
+];
+
+var servicesInfo = [
+    {
+        "name" : "Kafka Broker",
+        "port" : "9092",
+        "url"  : ""
+    },
+    {
+        "name" : "Schema Registry",
+        "port" : "8081",
+        "url"  : ""
+    },
+    {
+        "name" : "Kafka REST Proxy",
+        "port" : "8082",
+        "url"  : ""
+    },
+    {
+        "name" : "Kafka Connect Distributed",
+        "port" : "8083",
+        "url"  : ""
+    },
+    {
+        "name" : "ZooKeeper",
+        "port" : "2181",
+        "url"  : ""
+    },
+    {
+        "name" : "Web Server",
+        "port" : "3030",
+        "url"  : ""
+    }
+];
