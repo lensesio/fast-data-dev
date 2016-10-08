7456457424cd0197c857ec94a0ef21000d03500a
Marios Andreopoulos <opensource@andmarios.com>
2016-10-08 15:46:29 +0300

    Add undocumentend environment variable FORWARDLOGS which, when set to 0, makes fast-data-dev to not forward the logs to kafka (via connect). Intended for internal use for now.
    Signed-off-by: Marios Andreopoulos <opensource@andmarios.com>

--- logs-to-kafka.sh
+++ logs-to-kafka.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+FORWARDLOGS="${FORWARDLOGS:-1}"
 LOGS=(broker schema-registry rest-proxy connect-distributed zookeeper)
+
+if [[ "$FORWARDLOGS" == "0" ]]; then
+    echo "Skipping sinking logs to kafka  due to \$FORWARDLOGS = 0."
+    exit 0
+fi
+
 
 for (( i=0; i<=4; i++)); do
 cat <<EOF >/tmp/connector
