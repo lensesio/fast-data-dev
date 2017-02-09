0d8f9e765bb2efcee7961f486cf6618141d4f9cc
Marios Andreopoulos <opensource@andmarios.com>
2017-02-09 19:30:39 +0200

    Schema Registry UI: fix typo in env.js for global configuration.
    Signed-off-by: Marios Andreopoulos <opensource@andmarios.com>

--- web/registry-ui-env.js
+++ web/registry-ui-env.js
@@ -3,6 +3,6 @@ var clusters = [
     NAME: "fast-data-dev",
     SCHEMA_REGISTRY: "/api/schema-registry",
     COLOR: "#141414",
-    allowGlobalConfigChange: true
+    allowGlobalConfigChanges: true
   }
 ]
