fd094d329110dbf1f160055f5f5cc571f89437ae
Marios Andreopoulos <opensource@andmarios.com>
2017-04-17 13:35:35 +0300

    Use 5 partitions for position-reports.
    Signed-off-by: Marios Andreopoulos <opensource@andmarios.com>

--- sample-data/put.sh
+++ sample-data/put.sh
@@ -4,6 +4,13 @@ SAMPLEDATA="${SAMPLEDATA:-1}"
 if echo "$SAMPLEDATA" | egrep -sq "true|TRUE|y|Y|yes|YES|1"; then
     pushd /usr/share/landoop/sample-data
 
+    kafka-topics \
+        --zookeeper localhost:2181 \
+        --topic position-reports \
+        --partition 5 \
+        --replication 1 \
+        --create
+
     zcat ais.txt.gz | \
         kafka-avro-console-producer \
             --broker-list localhost:9092 \
