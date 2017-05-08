72cbee1c0cf5c302d21b0d59928e9ff554288510
Antonios Chalkiopoulos <antonios@landoop.com>
2017-05-08 23:36:23 +0100

    Adjusting poll timeout to 1,500 msec

--- web/topics-ui-env.js
+++ web/topics-ui-env.js
@@ -3,7 +3,7 @@ var clusters = [
     NAME: "fast-data-dev",
     KAFKA_REST: "/api/kafka-rest-proxy",
     MAX_BYTES: "50000",
-    RECORD_POLL_TIMEOUT: "5000",
+    RECORD_POLL_TIMEOUT: "1500",
     DEBUG_LOGS_ENABLED: true
   }
 ]
