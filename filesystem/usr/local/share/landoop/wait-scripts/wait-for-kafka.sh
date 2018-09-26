50d741bfe06378f0bd73bce717ea3dbe91b07004
Marios Andreopoulos <opensource@andmarios.com>
2018-09-27 02:29:19 +0300

    wait-scripts: fix script that waits for brokers, which had zk address hardcoded
    Signed-off-by: Marios Andreopoulos <opensource@andmarios.com>

--- filesystem/usr/local/share/landoop/wait-scripts/wait-for-kafka.sh
+++ filesystem/usr/local/share/landoop/wait-scripts/wait-for-kafka.sh
@@ -3,13 +3,13 @@
 W_ITERATIONS=${W_ITERATIONS:-60}
 W_PERIOD_SECS=${W_PERIOD_SECS:-2}
 W_BROKERS_WANTED=${W_BROKERS_WANTED:-1}
-W_ZK_ADDRESS=${w_ZK_ADDRESS:-localhost}
+W_ZK_ADDRESS=${W_ZK_ADDRESS:-localhost}
 W_ZK_PORT=${W_ZK_PORT:-$ZK_PORT}
 W_ZK_PORT=${W_ZK_PORT:-2181}
 
 for ((i=0;i<$W_ITERATIONS;i++)); do
     sleep $W_PERIOD_SECS
-    _BROKER_NUM="$(echo dump | nc localhost 2181|grep -c brokers/ids)"
+    _BROKER_NUM="$(echo dump | nc $W_ZK_ADDRESS $W_ZK_PORT | grep -c brokers/ids)"
     echo "Brokers detected/wanted: $_BROKER_NUM / $W_BROKERS_WANTED"
     if [[ ${_BROKER_NUM} -ge ${W_BROKERS_WANTED} ]]; then
         sleep 1
