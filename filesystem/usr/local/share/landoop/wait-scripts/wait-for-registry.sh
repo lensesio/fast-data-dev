db35d6a99a90a008c102d00651abc262b46f2e4b
Marios Andreopoulos <opensource@andmarios.com>
2018-09-29 19:43:24 +0300

    Fix wait-for-registry custom registry URL, add wait-for-connect script.
    Signed-off-by: Marios Andreopoulos <opensource@andmarios.com>

--- filesystem/usr/local/share/landoop/wait-scripts/wait-for-registry.sh
+++ filesystem/usr/local/share/landoop/wait-scripts/wait-for-registry.sh
@@ -2,7 +2,7 @@
 
 W_ITERATIONS=${W_ITERATIONS:-90}
 W_PERIOD_SECS=${W_PERIOD_SECS:-2}
-W_SR_ADDRESS=${w_SR_ADDRESS:-http://localhost:$REGISTRY_PORT}
+W_SR_ADDRESS=${W_SR_ADDRESS:-http://localhost:$REGISTRY_PORT}
 
 for ((i=0;i<$W_ITERATIONS;i++)); do
     sleep $W_PERIOD_SECS
