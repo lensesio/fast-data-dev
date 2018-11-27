b97002df35d917da8810cd1868b49d869e3b3507
Christos Kotsis <kotsis.chr@gmail.com>
2018-11-27 18:29:19 +0200

    removed if [[]] condition

--- filesystem/usr/local/share/landoop/sample-data/lenses-processor.sh
+++ filesystem/usr/local/share/landoop/sample-data/lenses-processor.sh
@@ -16,17 +16,9 @@ if [[ "${LENSES_PORT}" -ne '0' ]]; then
   # Wait for Lenses to get up if needed and see the topic
   for ((i=0;i<60;i++)); do
     sleep 5
-    if lenses-cli --timeout 3s --user "$USER" --pass "$PASSWORD" --host "${LEN_HOST}:$LENSES_PORT" topics | grep -sq "${SRC_TP}"; then
-      _col1="$(lenses-cli --timeout 3s --user "${USER}" --pass "${PASSWORD}" --host "${LEN_HOST}:$LENSES_PORT" topics \
-      | grep ${SRC_TP} | awk -F ' ' '{print $2}')"
-
-      _col2="$(lenses-cli --timeout 3s --user "${USER}" --pass "${PASSWORD}" --host "${LEN_HOST}:$LENSES_PORT" topics \
-      | grep ${SRC_TP} | awk -F ' ' '{print $3}')"
-
-      if [[ "${_col1}" == "${_col2}" ]]; then
-        unset _col1 _col2
-        sleep 5; break
-      fi
+    if lenses-cli --timeout 3s --user "${USER}" --pass "${PASSWORD}" --host "${LEN_HOST}:$LENSES_PORT" topics \
+    | grep ${SRC_TP} | grep -sqE "AVRO\s*AVRO"; then
+      sleep 5; break
     fi
   done
   lenses-cli --user "${USER}" --pass "${PASSWORD}" --host "${LEN_HOST}:3030" processor create --name=filter_fast_vessels --sql="${PROC_SQL}"
