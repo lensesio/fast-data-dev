d309a7883fd9dfad2354fd6f96b76e8766b0d333
Marios Andreopoulos <opensource@andmarios.com>
2018-11-27 19:39:26 +0200

    Small enhancements in the sample processor script.
    Signed-off-by: Marios Andreopoulos <opensource@andmarios.com>

--- filesystem/usr/local/share/landoop/sample-data/lenses-processor.sh
+++ filesystem/usr/local/share/landoop/sample-data/lenses-processor.sh
@@ -7,19 +7,30 @@ PASSWORD=${PASSWORD:-admin}
 LEN_HOST="http://localhost"
 SRC_TP="sea_vessel_position_reports"
 PROC_SQL="SET autocreate=true;
-INSERT INTO fast_vessel_processor
-SELECT MMSI, Speed, Longitude AS Long, Latitude AS Lat, \`Timestamp\`
-FROM ${SRC_TP}
-WHERE Speed > 10 AND _ktype=AVRO AND _vtype=AVRO"
 
-if [[ "${LENSES_PORT}" -ne '0' ]]; then
-  # Wait for Lenses to get up if needed and see the topic
-  for ((i=0;i<60;i++)); do
+INSERT INTO fast_vessel_processor
+    SELECT MMSI, Speed, Longitude AS Long, Latitude AS Lat, \`Timestamp\`
+    FROM ${SRC_TP}
+    WHERE Speed > 10 AND _ktype=AVRO AND _vtype=AVRO;"
+
+if [[ "${LENSES_PORT}" == "0" ]]; then
+    echo "Lenses is disabled. Skipping processor."
+    exit 0
+fi
+
+# Wait for Lenses to get up if needed and see the topic
+for ((i=0;i<60;i++)); do
     sleep 5
     if lenses-cli --timeout 3s --user "${USER}" --pass "${PASSWORD}" --host "${LEN_HOST}:$LENSES_PORT" topics \
-    | grep ${SRC_TP} | grep -sqE "AVRO\s*AVRO"; then
-      sleep 5; break
+            | grep ${SRC_TP} | grep -sqE "AVRO\s*AVRO"; then
+        sleep 5
+        break
     fi
-  done
-  lenses-cli --user "${USER}" --pass "${PASSWORD}" --host "${LEN_HOST}:3030" processor create --name=filter_fast_vessels --sql="${PROC_SQL}"
-fi
+done
+
+lenses-cli \
+    --user "${USER}" --pass "${PASSWORD}" --host "${LEN_HOST}:3030" \
+    processor \
+    create \
+    --name=filter_fast_vessels \
+    --sql="${PROC_SQL}"
