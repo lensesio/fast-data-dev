8212ef6076109319ce82bae2dbe9322a6356fd1e
Marios Andreopoulos <opensource@andmarios.com>
2020-11-16 18:35:25 +0200

    Fix hostnames in SSL certificate to include again localhost
    Signed-off-by: Marios Andreopoulos <opensource@andmarios.com>

--- setup-and-run.sh
+++ setup-and-run.sh
@@ -458,7 +458,7 @@ if [[ $ENABLE_SSL =~ $TRUE_REG ]]; then
             pushd /tmp/certs
             # Create Landoop Fast Data Dev CA
             quickcert -ca -out lfddca. -CN "Landoop's Fast Data Dev Self Signed Certificate Authority"
-            SSL_HOSTS="127.0.0.1,127.0.0.1,192.168.99.100"
+            SSL_HOSTS="localhost,127.0.0.1,192.168.99.100"
             HOSTNAME=${HOSTNAME:-} # This come from the container, so let's not permit it be unbound
             if [[ ! -z $HOSTNAME ]]; then SSL_HOSTS="$SSL_HOSTS,$HOSTNAME"; fi
             if [[ ! -z $ADV_HOST ]]; then SSL_HOSTS="$SSL_HOSTS,$ADV_HOST"; fi
